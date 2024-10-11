8a09bee7fb50d037be7ac0f578fbeef9f59fad50
WouterMathieuPXL <Wouter.Mathieu@student.pxl.be>
2024-10-11 19:22:23 +0200

    LAB 2: EmployeeTests

---
+++ backend-java/SpringCloudPXL/employee-service/src/test/java/be/pxl/services/employee/EmployeeTests.java
@@ -0,0 +1,109 @@
+package be.pxl.services.employee;
+
+import be.pxl.services.employee.domain.Employee;
+import be.pxl.services.employee.repository.EmployeeRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+public class EmployeeTests {
+
+    // uitvoeren in terminal: sudo ln -s $HOME/.docker/run/docker.sock /var/run/docker.sock
+    @Container
+    private static final MySQLContainer<?> sqlContainer = new MySQLContainer<>("mysql:8.0")
+            .withCommand("--default-authentication-plugin=mysql_native_password");
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    private Employee employee;
+
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", sqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", sqlContainer::getUsername);
+        registry.add("spring.datasource.password", sqlContainer::getPassword);
+    }
+
+    @BeforeEach
+    public void init() {
+        employeeRepository.deleteAll();
+
+        employee = Employee.builder()
+                .age(24)
+                .name("Jan")
+                .position("student")
+                .departmentId(1L)
+                .organizationId(2L)
+                .build();
+
+        employeeRepository.save(employee);
+    }
+
+    @Test
+    public void testCreateEmployee() throws Exception {
+        employeeRepository.deleteAll();
+
+        String employeeString = objectMapper.writeValueAsString(employee);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/employee")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(employeeString))
+                .andExpect(status().isCreated());
+
+        assertEquals(1, employeeRepository.findAll().size());
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/employee"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/employee/" + employee.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(24));
+    }
+
+    @Test
+    public void testFindByDepartment() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/employee/department/" + employee.getDepartmentId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    public void testFindByOrganization() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/employee/organization/" + employee.getOrganizationId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)));
+    }
+}
