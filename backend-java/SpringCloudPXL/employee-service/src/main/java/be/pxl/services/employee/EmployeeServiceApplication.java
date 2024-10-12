f79ed0eb3ee5b88c5a1c4dbb4f8589fa8320a4be
WouterMathieuPXL <Wouter.Mathieu@student.pxl.be>
2024-10-12 10:34:08 +0200

    LAB 3 + 4

--- backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/employee/EmployeeServiceApplication.java
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/employee/EmployeeServiceApplication.java
@@ -2,11 +2,13 @@ package be.pxl.services.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * EmployeeServiceApplication
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmployeeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
